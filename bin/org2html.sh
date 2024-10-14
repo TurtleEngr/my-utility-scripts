87c84f3ded149639d5da889aa8200f6435185ba1
TurtleEngr <turtle.engr@gmail.com>
2024-10-13 22:13:51 -0700

    Updated

--- bin/org2html.sh
+++ bin/org2html.sh
@@ -7,9 +7,12 @@ set -u
 # --------------------------------
 fUsage() {
     cat <<EOF
-org2html.sh - comvert FILE.org to FILE.html
+$cName - comvert FILE.org to FILE.html
 Usage
-    org2html.sh FILE.org FILE.html
+    $cName FILE.org FILE.html
+Debug
+    Before $cName is run, all files in $Tmp
+    are removed, unless env. var. gpDebug is set and not 0.
 EOF
     exit 1
 } # fUsage
@@ -17,19 +20,9 @@ EOF
 # ========================================
 # Main
 
+export gpDebug=${gpDebug:-0}
 cName=org2html.sh
-
-# -------------------
-# Get Args Section
-if [ $# -ne 2 ]; then
-    fUsage
-fi
-pFileIn=$1
-pFileOut=$2
-if [[ ! -r $pFileIn ]]; then
-    echo "Error: cannot read file: $pFileIn"
-    exit 1
-fi
+cTidyHtml="tidy -q -i -w 78 -asxhtml --break-before-br yes --indent-attributes yes --indent-spaces 2 --tidy-mark no --vertical-space no"
 
 # -------------------
 # Setup Tmp files
@@ -37,8 +30,9 @@ export Tmp=${Tmp:-"/tmp/$USER/$cName"}
 if [[ ! -d $Tmp ]]; then
     mkdir -p $Tmp
 fi
-# Only remove old files. Keep newest files.
-find $Tmp -mtime +2 -type f -exec rm {} \;
+if [[ $gpDebug -eq 0 ]]; then
+    rm $Tmp/* 2>/dev/null
+fi
 
 export cPID=$$
 export cTmpF=$Tmp/file-$cPID
@@ -50,26 +44,62 @@ cTmpErr=${cTmpF}-part3.err
 cPreFix=${cTmpF}-prefix.sed
 cPostFix=${cTmpF}-postfix.sed
 
-cTidyHtml="tidy -q -i -w 78 -asxhtml --break-before-br yes --indent-attributes yes --indent-spaces 2 --tidy-mark no --vertical-space no"
+# -------------------
+# Get Args Section
+if [ $# -ne 2 ]; then
+    fUsage
+fi
+
+pFileIn=$1
+pFileOut=$2
+if [[ ! -r $pFileIn ]]; then
+    echo "Error: cannot read file: $pFileIn"
+    exit 1
+fi
+
+# -------------------
+# Helper files
 
 cat <<\EOF >$cPreFix
 s/^ *- /\n\n/g
-s;^\*\*\*\* \(.*\);<h4>\1</h4>;
+s;^\*\*\*\* \(.*\);\n<h4>\1</h4>\n;
+s;^\*\*\*\*\* \(.*\);\n<h5>\1</h5>\n;
+s;\*\*\*\*\*\* \(.*\);\n<h6>\1</h6>\n;
 EOF
 
 cat <<\EOF >$cPostFix
 s/\&lt;/\</g
 s/\&gt;/>/g
+
+s/<p><p/<p/g
+s;</p></p>;</p>;g
+
 s/<p>\(<blockquote[^>]*>\)/\1<p>/
 s;</blockquote></p>;</p></blockquote>;
+
 s/\&quot;/"/g
 s;<p></p>;;g
 s;<p><div ;<div ;g
 s;</div></p>;</div>;g
-s;<p><h4>;<h4>;g
-s;</h4></p>;</h4>;g
+
 s|<p>&lt;h4&gt;|<h4>|g
 s|&lt;/h4&gt;</p>|</h4>|g
+
+s|<p>&lt;h5&gt;|<h5>|g
+s|&lt;/h5&gt;</p>|</h5>|g
+
+s|<p>&lt;h6&gt;|<h6>|g
+s|&lt;/h6&gt;</p>|</h6>|g
+
+s;<p><h4>;<h4>;g
+s;</h4></p>;</h4>;g
+
+s;<p><h5>;<h5>;g
+s;</h5></p>;</h5>;g
+
+s;<p><h6>;<h6>;g
+s;</h6></p>;</h6>;g
+
 s;{\(.\);<cite>{\1;g
 s;\(.\)};\1}</cite>;g
 s;<h2;<hr><h2;g
@@ -80,9 +110,12 @@ EOF
 
 sed -f $cPreFix  <$pFileIn >$cTmp1
 pandoc -f org -t html <$cTmp1 >$cTmp2
+
 sed -f /$cPostFix <$cTmp2 >$cTmp3
 $cTidyHtml <$cTmp3 >$pFileOut 2>$cTmpErr
+
 sed -i 's|/\*<!\[CDATA\[\*/|| ; s|/\*]]>\*/||' $pFileOut
+
 cat $cTmpErr
 echo
 echo "If lots of errors, see: $cTmp3 and $cTmpErr"
